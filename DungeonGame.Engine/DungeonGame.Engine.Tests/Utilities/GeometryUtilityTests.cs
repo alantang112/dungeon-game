91605eeb31455cddd3ff04a6d36008e07aee01eb
Alan <alantang112@gmail.com>
2026-03-17 12:40:00 +1300

    test cases

--- DungeonGame.Engine/DungeonGame.Engine.Tests/Utilities/GeometryUtilityTests.cs
+++ DungeonGame.Engine/DungeonGame.Engine.Tests/Utilities/GeometryUtilityTests.cs
@@ -3,10 +3,41 @@ namespace DungeonGame.Engine.Tests.Utilities;
 public class GeometryUtilityTests
 {
     #region CalculateDistanceBetween
-    [TestCase(1, 1, 1, 1, 0)]
-    // Add more test cases
-    public void CalculateDistanceBetween_Calculate(int x1, int y1, int x2, int y2, int expected)
+    
+    // same position
+    [TestCase(0, 0, 0)]
+    // orthogonal neighbour
+    [TestCase(0, 1, 2)]
+    [TestCase(0, -1, 2)]
+    [TestCase(1, 0, 2)]
+    [TestCase(-1, 0, 2)]
+    // diagonal neighbour
+    [TestCase(1, 1, 3)]
+    [TestCase(1, -1, 3)]
+    [TestCase(-1, 1, 3)]
+    [TestCase(-1, -1, 3)]
+    // orthogonal 
+    [TestCase(0, 2, 4)]
+    [TestCase(0, -3, 6)]
+    [TestCase(4, 0, 8)]
+    [TestCase(-5, 0, 10)]
+    // diagonal
+    [TestCase(2, 2, 6)]
+    [TestCase(3, -3, 9)]
+    [TestCase(-4, 4, 12)]
+    [TestCase(-5, -5, 15)]
+    // complex
+    [TestCase(1, 2, 5)]
+    [TestCase(3, 1, 7)]
+    [TestCase(4, -1, 9)]
+    [TestCase(2, -6, 14)]
+    [TestCase(-3, -8, 19)]
+    [TestCase(-10, -4, 24)]
+    [TestCase(-7, 3, 17)]
+    [TestCase(-2, 5, 12)]
+    public void CalculateDistanceBetween_Calculate(int xDelta, int yDelta, int expected)
     {
+        // TODO randomise the starting position
         throw new NotImplementedException();
     }
     #endregion
