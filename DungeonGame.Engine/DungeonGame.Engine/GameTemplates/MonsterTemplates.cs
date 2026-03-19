84a2c78cea262e3277bda5d10f7e56c00552a36c
Alan Tang <atang@barfoot.co.nz>
2026-03-19 16:24:24 +1300

    monster stats

--- DungeonGame.Engine/DungeonGame.Engine/GameTemplates/MonsterTemplates.cs
+++ DungeonGame.Engine/DungeonGame.Engine/GameTemplates/MonsterTemplates.cs
@@ -13,6 +13,11 @@ namespace DungeonGame.Engine.GameTemplates
             switch (type)
             {
                 case MonsterType.Spider:
+                    monster.Health = 2;
+                    monster.Stats.Add(SkillType.Movement, 5);
+                    monster.Stats.Add(SkillType.Attack, 4);
+                    monster.Stats.Add(SkillType.Defence, 4);
+                    monster.Stats.Add(SkillType.AttackRange, 3);
                     break;
                 default:
                     throw new NotImplementedException();
--- DungeonGame.Engine/DungeonGame.Engine/Models/Entities/Monster.cs
+++ DungeonGame.Engine/DungeonGame.Engine/Models/Entities/Monster.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using DungeonGame.Engine.Models.Enums;
 
 namespace DungeonGame.Engine.Models.Entities
@@ -5,5 +6,8 @@ namespace DungeonGame.Engine.Models.Entities
     public class Monster
     {
         public MonsterType Type { get; set; }
+
+        public int Health { get; set; }
+        public Dictionary<SkillType, int> Stats { get; set; }
     }
 }
