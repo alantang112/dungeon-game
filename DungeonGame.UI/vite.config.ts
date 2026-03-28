8a565669cac90035880d01aaa0a73e6b4c8c0504
Alan Tang <atang@barfoot.co.nz>
2026-03-28 16:51:00 +1300

    github pages setup

--- DungeonGame.UI/vite.config.ts
+++ DungeonGame.UI/vite.config.ts
@@ -3,9 +3,13 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/dungeon-game/' : '/',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
   ],
-})
+}));
