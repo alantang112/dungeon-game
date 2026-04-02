d788f08ad4c9748c129d807be256297a3c21ce0f
Alan Tang <atang@barfoot.co.nz>
2026-04-02 22:29:15 +1300

    update some names

--- DungeonGame.Engine/DungeonGame.Engine/Utilities/CharacterNameUtility.cs
+++ DungeonGame.Engine/DungeonGame.Engine/Utilities/CharacterNameUtility.cs
@@ -6,24 +6,24 @@ namespace DungeonGame.Engine.Utilities
     public class CharacterNameUtility
     {
         private static string[] MaleNames = { 
-            "Art", "Al", "Abe", "Ace", "Alf", "Arlo", "Ash", "Ben", "Bob", "Bill", 
+            "Arthur", "Alfred", "Abel", "Ace", "Alf", "Arlo", "Ash", "Ben", "Bob", "Bill", 
             "Brad", "Bud", "Bert", "Bart", "Baz", "Biff", "Buck", "Bo", "Bear", "Beau", 
-            "Cal", "Cam", "Carl", "Chad", "Chip", "Chuck", "Cliff", "Clint", "Clem", "Cole", 
+            "Callum", "Cameron", "Carl", "Chad", "Chip", "Chuck", "Cliff", "Clint", "Clem", "Cole", 
             "Cory", "Curt", "Cy", "Dan", "Dave", "Don", "Doug", "Drew", "Dax", "Dean", 
-            "Dex", "Dick", "Dolph", "Duff", "Duke", "Earl", "Ed", "Eli", "Edgy", "Erb", 
-            "Fred", "Frank", "Finn", "Fitz", "Floyd", "Fletch", "Gabe", "Gus", "Guy", "Gary", 
+            "Dex", "Dick", "Dolph", "Duff", "Duke", "Earl", "Edward", "Eli", "Edgy", "Erb", 
+            "Fred", "Frank", "Finn", "Fritz", "Floyd", "Fletch", "Gabe", "Gus", "Guy", "Gary", 
             "Gale", "Gene", "Glen", "Gil", "Gord", "Hank", "Hal", "Herb", "Hugh", "Hugo", 
-            "Ike", "Ian", "Ira", "Ivan", "Joe", "Jeff", "Jim", "Jack", "John", "Jake", 
+            "Ike", "Ian", "Isaac", "Ivan", "Joe", "Jeff", "Jim", "Jack", "John", "Jake", 
             "Jerry", "Jude", "Jeb", "Jed", "Jess", "Jett", "Jinx", "Jones", "Judd", "Ken", "Kurt", 
             "Karl", "Kev", "Kip", "Kirk", "Kyle", "Kane", "Kobe", "Len", "Lou", "Lee", 
             "Leo", "Lyle", "Linc", "Lance", "Lars", "Leif", "Max", "Matt", "Mike", "Mitch", 
             "Moe", "Milt", "Mack", "Mick", "Mel", "Mort", "Nate", "Ned", "Neil", "Nick", 
-            "Niles", "Nash", "Norm", "Noah", "Otis", "Oz", "Obe", "Oat", "Pat", "Paul", 
+            "Niles", "Nash", "Norm", "Noah", "Otis", "Oz", "Oscar", "Oat", "Pat", "Paul", 
             "Phil", "Pete", "Puck", "Pike", "Quinn", "Quent", "Ray", "Rich", "Rob", "Rick", 
             "Ron", "Russ", "Ralph", "Rex", "Rhett", "Rudy", "Reed", "Reid", "Sam", "Stan", 
-            "Sid", "Stu", "Sal", "Seth", "Saul", "Sly", "Skip", "Smith", "Spud", "Scott", "Sean",
+            "Sid", "Stanley", "Sal", "Seth", "Saul", "Sly", "Skip", "Smith", "Spud", "Scott", "Sean",
             "Steve", "Tom", "Tim", "Ted", "Todd", "Tex", "Ty", "Tate", "Toby", "Trent", "Troy", 
-            "Vic", "Vern", "Vince", "Van", "Vic", "Walt", "Wes", "Will", "Wade", "Ward", 
+            "Vic", "Vern", "Vince", "Van", "Vic", "Walt", "Wesley", "Will", "Wade", "Ward", 
             "Wyatt", "Wolf", "Zack", "Zeke", "Zane", "Zeb", "Zoy", "Alfie", "Bernie", 
             "Burt", "Chet", "Deke", "Ernie", "Fritz", "Gordie", "Horton", "Irwin", 
             "Jules", "Kelvin", "Lester", "Monty", "Nigel", "Oscar", "Percy", "Quig", 
@@ -38,7 +38,7 @@ namespace DungeonGame.Engine.Utilities
             "Kim", "Kat", "Kit", "Liz", "Lou", "Lynn", "Lois", "Liv", "Lana", "Lola", 
             "May", "Meg", "Mia", "Marge", "Mona", "Maud", "Nan", "Nell", "Nora", "Pam", 
             "Pat", "Peg", "Pearl", "Prue", "Rose", "Ruth", "Rae", "Rita", "Ruby", "Sue", 
-            "Sess", "Stacy", "Tess", "Trish", "Trix", "Val", "Viv", "Vi", "Vera", "Wanda", 
+            "Sess", "Stacy", "Tess", "Trish", "Trix", "Valerie", "Viv", "Vi", "Vera", "Wanda", 
             "Zoe", "Zelda", "Zita"
         };
 
