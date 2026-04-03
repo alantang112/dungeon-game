2f0c6d4a3175654cbdfa9feba87bdda6a95b258f
Alan Tang <atang@barfoot.co.nz>
2026-04-03 18:52:06 +1300

    fix flashing

--- DungeonGame.UI/src/props/Tile.tsx
+++ DungeonGame.UI/src/props/Tile.tsx
@@ -43,10 +43,12 @@ export const Tile = ({ x, y, tileType, name, heroCanWalk, heroCanAttack, health,
         } else if (entityChanged) {
             setIsAnimatingDamage(false);
         }
+    }, [health, identity]);
 
+    useEffect(() => {
         prevHealthRef.current = health;
         prevIdentityRef.current = identity;
-    }, [health, identity]);
+    }); // runs on every render
 
     return (
         <div className={"flex items-center justify-center border border-slate-700 bg-slate-800 text-slate-500 text-xs w-full h-16 sm:h-22 aspect-square relative "
