c38533a736ce92d1369b451b2b2c9796d63ef004
Alan <alantang112@gmail.com>
2026-04-05 23:29:56 +1200

    howtoplay assets

--- DungeonGame.UI/src/HowToPlayContent.ts
+++ DungeonGame.UI/src/HowToPlayContent.ts
@@ -1,3 +1,5 @@
+import { assetPath, howToPlayAssetsPath } from "./constants/assetConstants";
+
 export interface HelpItem {
     type: 'heading' | 'text' | 'image' | 'separator';
     content: string; // The text content or the image src URL
@@ -17,7 +19,7 @@ export const howToPlayPages: HelpPage[] = [
         items: [
             { type: 'text', content: "This is a turn based action game played on a 5x5 grid." },
             { type: 'text', content: 'Each turn consists of 1) rolling energy dice, 2) your actions (moving and attacking), and 3) monster actions (moving and attacking).'},
-            { type: 'image', content: '/howtoplay/overview-1.png'},
+            { type: 'image', content: `${assetPath}${howToPlayAssetsPath}overview-1.png`},
         ]
     },
     {
@@ -25,9 +27,9 @@ export const howToPlayPages: HelpPage[] = [
         title: "Energy dice",
         items: [
             { type: 'text', content: "Each round, you roll 3 dice and assign them to Movement, Attack or Defence (one die per stat)." },
-            { type: 'image', content: '/howtoplay/energydice-1.png'},
+            { type: 'image', content: `${assetPath}${howToPlayAssetsPath}energydice-1.png`},
             { type: 'text', content: "Energy points = Base stat + Energy dice."},
-            { type: 'image', content: '/howtoplay/stats-1.png'},
+            { type: 'image', content: `${assetPath}${howToPlayAssetsPath}stats-1.png`},
         ]
     },
     {
@@ -35,7 +37,7 @@ export const howToPlayPages: HelpPage[] = [
         title: "Moving Your Hero",
         items: [
             { type: 'text', content: "Click on any tile adjacent to your hero to move." },
-            { type: 'image', content: '/howtoplay/movement-1.png' },
+            { type: 'image', content: `${assetPath}${howToPlayAssetsPath}movement-1.png` },
             { type: 'text', content: "It costs 2 movement points to move orthogonally and 3 movement points to move diagonally." },
             { type: 'text', content: "You can't hop diagonally through walls." },
             { type: 'text', content: "You can hop diagonally past monsters." },
@@ -49,7 +51,7 @@ export const howToPlayPages: HelpPage[] = [
             { type: 'text', content: "You spend attack energy equal to the monster's defence stat to deal 1 damage." },
             { type: 'heading', content: "Range" },
             { type: 'text', content: "Range is determined the same way as movement; attacking an orthogonal tile requires 2 attack range." },
-            { type: 'image', content: 'howtoplay/attack-1.png' }
+            { type: 'image', content: `${assetPath}${howToPlayAssetsPath}attack-1.png` }
         ]
     },
     {
@@ -59,8 +61,8 @@ export const howToPlayPages: HelpPage[] = [
             { type: 'heading', content: "Line of sight" },
             { type: 'text', content: "You have line of sight if you can draw an uninterrupted line between any corner of your tile to the target tile." },
             { type: 'text', content: "You don't have line of sight through walls and monsters." },
-            { type: 'image', content: 'howtoplay/lineofsight-1.png' },
-            { type: 'image', content: 'howtoplay/lineofsight-2.png' }
+            { type: 'image', content: `${assetPath}${howToPlayAssetsPath}lineofsight-1.png` },
+            { type: 'image', content: `${assetPath}${howToPlayAssetsPath}lineofsight-2.png` }
         ]
     },
     {
@@ -71,7 +73,7 @@ export const howToPlayPages: HelpPage[] = [
             { type: 'text', content: "Monsters move one at a time, in order of distance from you, starting with the closest." },
             { type: 'text', content: "They will move towards the tile at their maximum range from you with line of sight. If there are no tiles in range and in line of sight, they will move as close as possible to you." },
             { type: 'text', content: "If there are multiple options, they will prioritise the closest, then if still tied the leftmost tile, then the bottommost tile." },
-            { type: 'image', content: 'howtoplay/monstermove-1.png' },
+            { type: 'image', content: `${assetPath}${howToPlayAssetsPath}monstermove-1.png` },
             { type: 'text', content: "In the above example, Ty moves to be at range 3 because Spiders have 3 attack range, while Ruby moves to be at range 3 top-right of the hero since it requires the least movement compared to top-left of the hero." }
         ]
     },
@@ -80,7 +82,7 @@ export const howToPlayPages: HelpPage[] = [
         title: "Monster Attack",
         items: [
             { type: 'text', content: "After all monsters have moved, monsters with enough attack range and line of sight of you, attack." },
-            { type: 'image', content: 'howtoplay/monsterattack-1.png' },
+            { type: 'image', content: `${assetPath}${howToPlayAssetsPath}monsterattack-1.png` },
             { type: 'text', content: "Damage taken = (sum of attacking monster's attack) / (your defence energy), rounded down." },
         ]
     },
@@ -89,7 +91,7 @@ export const howToPlayPages: HelpPage[] = [
         title: "Level end",
         items: [
             { type: 'text', content: "The level ends after you have defeated all monsters." },
-            { type: 'image', content: 'howtoplay/levelend-1.png' },
+            { type: 'image', content: `${assetPath}${howToPlayAssetsPath}levelend-1.png` },
             { type: 'text', content: "Upgrade a base stat or replenish your health to full as a reward before proceeding to the next level." },
         ]
     }
--- DungeonGame.UI/src/constants/assetConstants.ts
+++ DungeonGame.UI/src/constants/assetConstants.ts
@@ -1,3 +1,4 @@
 export const assetPath: string = "./assets/";
+export const howToPlayAssetsPath: string = "howtoplay/"
 export const diceFileName: string = "dice";
 export const d4FileName: string = "d4-";
