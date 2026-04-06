a52702ae0e95796e83bfc237077738b0a11c54da
Alan <alantang112@gmail.com>
2026-04-07 09:37:35 +1200

    bump version

--- DungeonGame.UI/src/constants/gameConstants.ts
+++ DungeonGame.UI/src/constants/gameConstants.ts
@@ -1,6 +1,6 @@
 import type { SkillType } from "../models/GameEngineModels";
 
-export const UIVersion: string = '1.2.3';
+export const UIVersion: string = '1.2.4';
 
 export const DebugMode: boolean = false; 
 
