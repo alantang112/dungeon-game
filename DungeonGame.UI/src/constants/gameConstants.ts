3292217701661fd27ba08142960b207c47572600
alantang112 <alantang112@gmail.com>
2026-04-06 22:06:00 +1200

    Update gameConstants.ts

--- DungeonGame.UI/src/constants/gameConstants.ts
+++ DungeonGame.UI/src/constants/gameConstants.ts
@@ -1,6 +1,6 @@
 import type { SkillType } from "../models/GameEngineModels";
 
-export const UIVersion: string = '1.2.0';
+export const UIVersion: string = '1.2.1';
 
 export const DebugMode: boolean = false; 
 
